9e3bf1655307dd3359d9a3429a91e8cce60d2a8d
Vanessa Tomaz <vanessatomaz246@gmail.com>
2022-09-18 14:48:35 -0300

    Criada a camada de serviço, recupera todos os usuarios e um id especifico

--- src/main/java/com/example/curso/resourses/UserResource.java
+++ src/main/java/com/example/curso/resourses/UserResource.java
@@ -1,20 +1,40 @@
 package com.example.curso.resourses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.curso.entities.User;
+import com.example.curso.services.UserServices;
 
 @RestController
 @RequestMapping(value= "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserServices service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L,"Maria","maria@gmaiçom","9294859203","1234");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
+
+
+
+
+
 
